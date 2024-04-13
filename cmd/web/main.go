e14e1c5378dcafd048d72351e0cc4e97142f78ce
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-04-13 16:42:29 +1000

    logger and configuration flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	const port = ":4000"
+	// Configuration
+	addr := flag.String("addr", ":8080", "HTTP network address")
+	staticDir := flag.String("staticDir", "./ui/static/", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 
-	// File server to serve files out of ui/static directory
-	fileServer := http.FileServer(safeFileSystem{http.Dir("./ui/static/")})
+	// File server to serve static files
+	fileServer := http.FileServer(safeFileSystem{http.Dir(*staticDir)})
 	mux.Handle("GET /static", http.NotFoundHandler())
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
@@ -22,8 +31,8 @@ func main() {
 	mux.HandleFunc("GET /posts", posts)
 	mux.HandleFunc("GET /projects", projects)
 
-	log.Printf("Starting server on %q\n", port)
+	log.Printf("Starting server on %q\n", *addr)
 
-	err := http.ListenAndServe(port, mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
