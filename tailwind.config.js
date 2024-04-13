2c192451a4a562cf63811bb94ab789398ce112a5
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-04-13 10:06:44 +1000

    add tailwind and setup fileserver for static content

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./ui/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+          sans: ["Iosevka Aile Iaso", "sans-serif"],
+          mono: ["Iosevka Curly Iaso", "monospace"],
+          serif: ["Iosevka Etoile Iaso", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
