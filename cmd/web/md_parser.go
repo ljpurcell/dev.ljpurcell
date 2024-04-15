64d33842b76f3a906c6e92cc123a273f16138598
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-04-16 08:49:16 +1000

    basic parsing of markdown front-matter

--- cmd/web/md_parser.go
+++ cmd/web/md_parser.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 )
+
+func parseMdFile(content []byte, p *post, md *[]byte) error {
+	c := string(content)
+	seperators := 2
+	if !strings.HasPrefix(c, "---") {
+		seperators = 1
+	}
+	sections := strings.SplitN(c, "---", seperators+1)
+
+	metaBlock, mu := sections[len(sections)-2], sections[len(sections)-1]
+
+	metaLines := strings.Split(metaBlock, "\n")
+
+	metaMap := make(map[string]string)
+	for _, line := range metaLines {
+		fieldVal := strings.SplitN(line, ":", 2)
+		if len(fieldVal) == 2 {
+			field := strings.ToLower(strings.TrimSpace(fieldVal[0]))
+			value := strings.TrimSpace(fieldVal[1])
+			metaMap[field] = value
+		}
+	}
+
+	p.title = metaMap["title"]
+	p.slug = metaMap["slug"]
+	p.catergory = metaMap["catergory"]
+
+	*md = mdToHTML([]byte(mu))
+	return nil
+}
 
 func mdToHTML(md []byte) []byte {
 	// Create MD parser with extensions
