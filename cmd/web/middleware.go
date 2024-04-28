fb6f372f6af91c761139667a83689b383527c1b1
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-04-28 11:03:17 +1000

    refactor CSP header and add nonce for scripts

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,15 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
 
-func attachCommonHeaders(next http.Handler) http.HandlerFunc {
+func (app *application) commonHeaders(next http.Handler) http.HandlerFunc {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' cdn.xeiaso.net")
+
+		nonce, err := app.generateNonce()
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), nonceKey, nonce)
+
+		csp := fmt.Sprintf("default-src 'self'; style-src 'self' cdn.xeiaso.net; font-src 'self' data:; script-src 'self' 'nonce-%s'", nonce)
+
+		w.Header().Set("Content-Security-Policy", csp)
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
