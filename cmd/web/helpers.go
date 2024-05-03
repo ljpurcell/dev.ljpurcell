b5e38a910ea8b7702485a6e67167cd72eeb18477
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-05-03 18:06:59 +1000

    brotli and gzip compressed responses

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	"compress/gzip"
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/andybalholm/brotli"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -33,6 +37,32 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 	err := ts.ExecuteTemplate(&buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	encodings := r.Header.Get("Accept-Encoding")
+
+	if strings.Contains(encodings, brEncoding) {
+		w.Header().Set("Content-Encoding", brEncoding)
+		w.Header().Set("Vary", "Content-Encoding")
+		w.WriteHeader(status)
+
+		bw := brotli.NewWriter(w)
+		defer bw.Close()
+
+		buf.WriteTo(bw)
+		return
+	} else if strings.Contains(encodings, gzipEncoding) {
+		w.Header().Set("Content-Encoding", gzipEncoding)
+		w.Header().Set("Vary", "Content-Encoding")
+		w.WriteHeader(status)
+
+		zw := gzip.NewWriter(w)
+		defer zw.Close()
+
+		buf.WriteTo(zw)
 		return
 	}
 
